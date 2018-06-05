63c0c03817075d7d498bd7990f14e56cced94097
Kamil <kamilbajdo@gmail.com>
2018-06-05 11:59:10 +0200

    Back button from main panel wont go back to login activity

--- app/src/main/java/com/example/kamil/smartrpi/LoginActivity.java
+++ app/src/main/java/com/example/kamil/smartrpi/LoginActivity.java
@@ -26,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
         });
