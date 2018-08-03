1399b98b881de22f61a1311b5238cdc51d19f935
Kamil <kamilbajdo@gmail.com>
2018-08-03 15:59:56 +0200

    menu toolbar options + websocket recyclerview disply sensors (need to add handlers for optimisation)

--- app/src/main/java/com/example/kamil/smartrpi/MainActivity.java
+++ app/src/main/java/com/example/kamil/smartrpi/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.kamil.smartrpi;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,18 +15,28 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
-import com.example.kamil.smartrpi.adapter.MyAdapter;
-import com.example.kamil.smartrpi.models.Article;
+import com.example.kamil.smartrpi.adapter.SensorAdapter;
+import com.example.kamil.smartrpi.models.*;
 import com.example.kamil.smartrpi.websocket.WSService;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity implements WSService.Callbacks {
-    private String device_key;
+    private String loggingSessionKey;
     private Toolbar toolbar;
     private Intent serviceIntent;
     private WSService wsService;
+    private ArrayList<Sensor> sensors = new ArrayList<>();
+    private SensorAdapter sensorAdapter = new SensorAdapter();
+    @Bind(R.id.recycler) RecyclerView recyclerView;
+
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName className, IBinder service) {
@@ -42,31 +54,29 @@ public class MainActivity extends AppCompatActivity implements WSService.Callbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        device_key = getIntent().getStringExtra("UUID_KEY");
-        System.out.println(device_key);
-
-        //SERVICE
-        serviceIntent = new Intent(MainActivity.this, WSService.class);
-        serviceIntent.putExtra("UUID_KEY", device_key);
-        startService(serviceIntent);
-        bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE);
-
-        // RECYCLER VIEW
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        ArrayList<Article> articles = new ArrayList<>();
-        for (int i = 0; i < 20; ++i) {
-            articles.add(new Article());
-        }
-
-        recyclerView.setAdapter(new MyAdapter(articles, recyclerView));
+        ButterKnife.bind(this);
+        loggingSessionKey = getIntent().getStringExtra("UUID_KEY");
+        System.out.println(loggingSessionKey);
 
         //TOP BAR MENU
         toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+
+        //SERVICE
+        serviceIntent = new Intent(MainActivity.this, WSService.class);
+        serviceIntent.putExtra("UUID_KEY", loggingSessionKey);
+        startService(serviceIntent);
+        bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE);
+
+        // RECYCLER VIEW
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        sensorAdapter.setmSensors(sensors);
+        sensorAdapter.setmRecyclerView(recyclerView);
+        recyclerView.setAdapter(sensorAdapter);
+
     }
 
     public void startWsService(){
@@ -99,6 +109,9 @@ public class MainActivity extends AppCompatActivity implements WSService.Callbac
         if (id == R.id.action_refresh) {
             stopWsService();
             startWsService();
+            wsService.getAllSensors();
+
+
             return true;
         }
         if (id == R.id.action_about){
@@ -110,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements WSService.Callbac
             return true;
         }
         if (id == R.id.action_add_device){
-            wsService.sensorPageRefresh();
+            addDevice();
             //TODO add device to user
             return true;
         }
@@ -126,11 +139,85 @@ public class MainActivity extends AppCompatActivity implements WSService.Callbac
     }
 
     @Override
-    public void updateClient(Integer data){
+    public void updateClient(Payload payload){
     }
 
     @Override
     public void changeBtnName(String name){
+    }
+
+    @Override
+    public void updateRecyclerView(List<Sensor> sensory){
+//        sensors = new ArrayList<>();
+//        for (Sensor s : sensory){
+//            sensors.add(s);
+//            sensorAdapter.notifyItemInserted(sensors.size()-1);
+//
+//            if (sensor instanceof TemperatureSensor) {
+//                System.out.println("yea");
+//            } else {
+//                System.out.println("nope");
+//            }
+        for (Sensor s : sensors){
+            Integer i = sensors.indexOf(s);
+            sensors.remove(s);
+            sensorAdapter.notifyItemRemoved(i);
+            sensorAdapter.notifyDataSetChanged();
+        }
+        System.out.println(sensors.size());
+
+        for (Sensor s : sensory){
+            sensors.add(s);
+            System.out.println("item count " +sensorAdapter.getItemCount());
+            System.out.println("kafelek " + s.getName() + s.getOwnerDevice() + ((TemperatureSensor)s).getTemp() );
+            sensorAdapter.notifyItemInserted(sensors.size()-1);
+        }
+
+
+
+
+//        sensors.removeAll(sensors);
+//        sensorAdapter.setmSensors(sensors);
+//        System.out.println("sad");
+//        sensorAdapter.notifyDataSetChanged();
+//        System.out.println("sad");
+//        for (Sensor s : sensory){
+//            sensors.add(s);
+//            sensorAdapter.notifyItemInserted(sensors.size()-1);
+//        }
+
+    }
+
+    public void addToRecyclerView(Device device){
+        if (!device.getSensorList().isEmpty()){
+            sensors.addAll(device.getSensorList());
+            recyclerView.setAdapter(new SensorAdapter(sensors, recyclerView));
+        }
+    }
+
+    public void addDevice(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+
+        alert.setTitle("Add device");
+        alert.setMessage("Type in the access key of your device.");
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                wsService.addNewDevice(input.getText().toString());
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Back to the main view
+            }
+        });
+
+        alert.show();
     }
 
     @Override
